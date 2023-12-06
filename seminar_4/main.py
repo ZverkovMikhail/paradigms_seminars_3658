4f7a35f85b4b96ca4d477cdc916dd1e3cb5d26fe
Ches613 <zverkovmi@gmail.com>
2023-12-06 14:39:18 +0400

    added homework for seminar 4

--- seminar_4/main.py
+++ seminar_4/main.py
@@ -1,7 +1,12 @@
 import statistics
 
+
+def correlation(arr1, arr2):
+    return round(statistics.correlation(arr1, arr2), 4)
+
+
 if __name__ == '__main__':
     orbital_period = [88, 225, 365, 687, 4331, 10756, 30_687, 60190]
     dist_from_sun = [58, 108, 150, 228, 778, 1400, 2900, 4500]
 
-    print(round(statistics.correlation(orbital_period, dist_from_sun), 4))
+    print(correlation(orbital_period, dist_from_sun))
